3376828a13548f59a63a45140b7cc6c6a41bf4d3
not a dog <assaf.treves@gmail.com>
2022-04-18 12:51:52 +0300

    parser

---
+++ assembler/main.java
@@ -0,0 +1,9 @@
+/**
+ * main
+ */
+public class main {
+
+public static void main(String[] args) {
+    
+}    
+}
---
+++ assembler/parser.java
@@ -0,0 +1,120 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class parser {
+    BufferedReader bufferedReader = null;
+    public int lineindex;
+
+    public parser() throws FileNotFoundException {
+
+        Scanner reader = new Scanner(System.in);
+        String fileName = reader.nextLine();
+        FileReader fileReader = new FileReader(fileName);
+        bufferedReader = new BufferedReader(fileReader);
+    }
+
+    public String readline(int num) throws IOException {
+        String line = "";
+        for (int i = 0; i < num; i++) {
+            line = bufferedReader.readLine();
+
+        }
+        return line;
+
+    }
+
+    public boolean hasMoreLines() throws IOException {
+        if (bufferedReader.readLine() != null) {
+            return true;
+        }
+        return false;
+    }
+
+    public void advance() throws IOException {
+        if (hasMoreLines()) {
+            String line = readline(lineindex);
+            for (int i = 0; i < line.length(); i++) {
+                if (line.charAt(i + 1) == line.charAt(i) && line.charAt(i) == '/') {
+                    lineindex++;
+                }
+            }
+        }
+    }
+
+    public String instactionType() throws IOException {
+        String line = readline(lineindex);
+        for (int j = 0; j < line.length(); j++) {
+            if (line.charAt(j) == '(') {
+                return "L";
+            }
+            if (line.charAt(j) == '@') {
+                return "A";
+            }
+            if (line.charAt(j) == '=') {
+                return "C";
+            }
+
+        }
+        return null;
+
+    }
+
+    public String symbol() throws IOException {
+        String line = readline(lineindex);
+        String newLine = "";
+        for (int i = 0; i < line.length(); i++) {
+
+            if (instactionType() == "L") {
+                if(line.charAt(i) != '(' || line.charAt(i) != ')') {
+                    line = line.substring(0, i) + line.substring(i + 1);
+                }
+            }
+            if (instactionType() == "A") {
+                if(line.charAt(i) != '@') {
+                    line = line.substring(0, i) + line.substring(i + 1);
+                }
+            }
+        }
+        return line;
+    }
+    public String dest() throws IOException {
+        String line = readline(lineindex);
+        int i = 0;
+        while(line.charAt(i)  ==  ' ') {
+            i++;
+        }
+        return "" + line.charAt(i);
+    }
+    public String comp() throws IOException {
+        String line = readline(lineindex);
+        String newLine = "";
+        int i = 0;
+        while(line.charAt(i)  ==  '=') {
+            i++;
+        }
+        int j = i;
+        i = 0;
+        while(line.charAt(i)  ==  ';') {
+            i++;
+        }
+        while (i < j) {
+            newLine = newLine + line.charAt(i);
+        }
+        newLine = newLine + line.charAt(j);
+        return newLine;
+    }
+    public String jump() throws IOException {
+        String line = readline(lineindex);
+        String newLine = "";
+        int i = 0;
+        int j = line.length();
+        while(line.charAt(i)  ==  ';') {
+            i++;
+        }
+        while (i < j) {
+            newLine = newLine + line.charAt(i);
+        }
+        newLine = newLine + line.charAt(j);
+        return newLine;
+    }
+}
